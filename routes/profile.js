ff38d30fd3f1974970a92566871d11b2800d18ab
João Maria <joao.ftf.maria@gmail.com>
2020-08-03 17:46:10 +0100

    merge

--- routes/profile.js
+++ routes/profile.js
@@ -7,23 +7,27 @@ const routeGuard = require('./../middleware/route-guard');
 const profileRouter = new express.Router();
 
 profileRouter.get('/:id', routeGuard, (req, res) => {
-    res.render('profile');
+    res.render('profile/profile');
   });
+  profileRouter.get('/edit', routeGuard, (request, response, next) => {
+    response.render('/profile/edit');
+  });
+
   profileRouter.get('/edit', routeGuard, (request, response, next) => {
     response.render('profile/edit');
   });
-
+  
   profileRouter.post('/edit', routeGuard, (request, response, next) => {
     const id = request.session.userId;
     const { name, email } = request.body;
-
+  
     User.findByIdAndUpdate(id, { name, email })
-    .then(() => {
-      response.redirect('/profile');
-    })
-    .catch(error => {
-      next(error);
-    });
-});
+      .then(() => {
+        response.redirect('/profile');
+      })
+      .catch(error => {
+        next(error);
+      });
+  });
 
 module.exports = profileRouter;
