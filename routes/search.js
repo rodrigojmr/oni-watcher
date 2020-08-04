fc7d05eef0ee8bf9a9d5e4dbc4142284d2d4dd61
Rodrigo Moura <rodrigojorgemoura@gmail.com>
2020-08-04 15:25:49 +0100

    Refactor setSeason function

--- routes/search.js
+++ routes/search.js
@@ -6,6 +6,27 @@ const Kitsu = require('kitsu');
 
 const api = new Kitsu();
 
+const setSeason = month => {
+  switch (month) {
+    case 1:
+    case 2:
+    case 3:
+      return 'Winter';
+    case 4:
+    case 5:
+    case 6:
+      return 'Spring';
+    case 7:
+    case 8:
+    case 9:
+      return 'Summer';
+    case 10:
+    case 11:
+    case 12:
+      return 'Autumn';
+  }
+};
+
 searchRouter.post('/', async (req, res, next) => {
   const { name } = req.body;
 
@@ -16,33 +37,10 @@ searchRouter.post('/', async (req, res, next) => {
       }
     });
     for (const anime of data) {
-      anime.year = anime.startDate.split('-')[0];
-
       const month = parseInt(anime.startDate.split('-')[1]);
-      let season = '';
-      switch (month) {
-        case 1:
-        case 2:
-        case 3:
-          season = 'Winter';
-          break;
-        case 4:
-        case 5:
-        case 6:
-          season = 'Spring';
-          break;
-        case 7:
-        case 8:
-        case 9:
-          season = 'Summer';
-          break;
-        case 10:
-        case 11:
-        case 12:
-          season = 'Autumn';
-          break;
-      }
-      anime.season = season;
+
+      anime.year = anime.startDate.split('-')[0];
+      anime.season = setSeason(month);
     }
     // console.log(data);
     res.render('index', { anime: data });
