68e9db16df0b4c267daf27d41fa2dc6aa8a917d7
Rodrigo Moura <stormofthunder@bitbucket.org>
2020-08-07 16:32:19 +0100

    Fix

--- routes/index.js
+++ routes/index.js
@@ -4,25 +4,22 @@ const { Router } = require('express');
 const router = new Router();
 const routeGuard = require('./../middleware/route-guard');
 
-
 router.get('/', (req, res, next) => {
+  // try {
+  //   const { data } = await api.get('anime');
 
-  try {
-    const { data } = await api.get('anime');
-
-    console.log(data[0]);
-    for (const anime of data) {
-      const month = parseInt(anime.startDate.split('-')[1]);
-      anime.year = anime.startDate.split('-')[0];
-      anime.season = setSeason(month);
-    }
-    res.render('search', { anime: data });
-  } catch (error) {
-    next(error);
-  }
+  //   console.log(data[0]);
+  //   for (const anime of data) {
+  //     const month = parseInt(anime.startDate.split('-')[1]);
+  //     anime.year = anime.startDate.split('-')[0];
+  //     anime.season = setSeason(month);
+  //   }
+  //   res.render('search', { anime: data });
+  // } catch (error) {
+  //   next(error);
+  // }
   res.render('index', { title: 'Oni-Watch' });
 });
-
 
 const roleRouteGuard = require('./../middleware/role-route-guard');
 
@@ -34,7 +31,5 @@ router.get(
     res.render('User');
   }
 );
-
-
 
 module.exports = router;
