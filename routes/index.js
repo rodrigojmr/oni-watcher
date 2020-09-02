20b34eab8766b525f89df3f1a71cd696b004416d
Rodrigo Moura <rodrigojorgemoura@gmail.com>
2020-09-02 02:39:26 +0100

    List trending and top airing in home

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,41 @@ const { Router } = require('express');
 const router = new Router();
 const routeGuard = require('./../middleware/route-guard');
 
-router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Oni-Watch' });
+const axios = require('axios');
+const Kitsu = require('kitsu');
+const api = new Kitsu({ pluralize: false });
+
+router.get('/', async (req, res, next) => {
+  try {
+    const trending = await api.get('trending/anime', {
+      page: { limit: 6 }
+    });
+    const sixTrending = trending.data.filter((e, i) => i <= 5);
+
+    // const topRated = await api.get('anime', {
+    //   page: { limit: 6 },
+    //   sort: 'ratingRank'
+
+    // });
+
+    const current = await api.get('anime', {
+      page: { limit: 6 },
+      filter: {
+        status: 'current'
+      },
+      sort: '-averageRating'
+    });
+
+    const data = {
+      title: 'Oni-Watchers',
+      trending: sixTrending,
+      current: current.data
+    };
+
+    res.render('index', data);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 const roleRouteGuard = require('./../middleware/role-route-guard');
